6e35dc7cb7fee2203fd00ca807aa7a3ac6177ceb
Wissem Jderi <asqr43641@gmail.com>
2025-03-15 10:50:15 +0100

    Added flexbox styling to the Flashcard component.

--- src/components/FlashCard.jsx
+++ src/components/FlashCard.jsx
@@ -6,14 +6,10 @@ function FlashCard({ question, answer, category }) {
 
   return (
     <div className={`card-container ${showAnswer ? "show" : "hide"}`}>
-      {!showAnswer ? (
-        <>
-          <h2>The Question: {question}</h2>
-          <h3>The Category: {category}</h3>
-        </>
-      ) : (
-        <h2> The Answer: {answer}</h2>
-      )}
+      <h2>The Question: {question}</h2>
+      {!showAnswer ? <></> : <h2> The Answer: {answer}</h2>}
+      <h3>The Category: {category}</h3>
+
       <button
         onClick={() => {
           setShowAnswer(!showAnswer);
