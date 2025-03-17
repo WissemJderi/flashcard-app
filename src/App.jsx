95583494cd8654090608713660c6f0bcb2e8f5e2
Wissem Jderi <asqr43641@gmail.com>
2025-03-17 11:34:05 +0100

    Added filtering for flashcards.

--- src/App.jsx
+++ src/App.jsx
@@ -19,27 +19,13 @@ function App() {
     "Science",
     "DSA",
   ]);
-  const [isShown, setIsShown] = useState(true);
+  const [isShown, setIsShown] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState("");
 
   useEffect(() => {
     // Save flashcards to local storage whenever they change
     localStorage.setItem("flashCards", JSON.stringify(flashCards));
   }, [flashCards]);
-
-  let flashCardsList = flashCards.map((flashCard) => {
-    return (
-      <div className="flashCards-container" key={flashCard.question}>
-        <FlashCard
-          question={flashCard.question}
-          category={flashCard.category}
-          answer={flashCard.answer}
-          deleteCard={deleteCard}
-        />
-      </div>
-    );
-  });
-
-  // Functions
 
   function addNewFlashCard() {
     setIsShown(!isShown);
@@ -58,13 +44,55 @@ function App() {
     });
   }
 
+  function filterCards(cat) {
+    setSelectedCategory(cat);
+  }
+
+  const filteredFlashCards =
+    selectedCategory && selectedCategory !== "See All"
+      ? flashCards.filter(
+          (flashCard) => flashCard.category === selectedCategory
+        )
+      : flashCards;
+
+  const flashCardsList = filteredFlashCards.map((flashCard) => {
+    return (
+      <div className="flashCards-container" key={flashCard.question}>
+        <FlashCard
+          question={flashCard.question}
+          category={flashCard.category}
+          answer={flashCard.answer}
+          deleteCard={deleteCard}
+        />
+      </div>
+    );
+  });
+
+  const categoriesSpans = [...categories, "See All"].map((category) => {
+    return (
+      <span
+        className="category-span"
+        onClick={() => filterCards(category)}
+        key={category}
+      >
+        {category}
+      </span>
+    );
+  });
+
   return (
     <>
       <Header addNewFlashCard={addNewFlashCard} />
       {isShown ? (
         <FlashCardInput categories={categories} addNewCard={addNewCard} />
       ) : null}
-      {flashCardsList}
+      <h3 className="info">Filter By Category: </h3>
+      <div className="categories-container">{categoriesSpans}</div>
+      {flashCards.length === 0 ? (
+        <h1 className="info">You Have No Flash Cards...</h1>
+      ) : (
+        flashCardsList
+      )}
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,26 +3,16 @@ function Header({ addNewFlashCard }) {
   return (
     <div className="header">
       <h1 className="logo">Flash Card Application</h1>
-      <div className="header-btns">
-        <button
-          className="add-flash-card"
-          onClick={() => {
-            addNewFlashCard();
-          }}
-        >
-          {" "}
-          Add A New Flash Card
-        </button>
-        <button
-          className="add-flash-card"
-          onClick={() => {
-            console.log("Working");
-          }}
-        >
-          {" "}
-          Manage Your Flash Cards
-        </button>
-      </div>
+
+      <button
+        className="add-flash-card"
+        onClick={() => {
+          addNewFlashCard();
+        }}
+      >
+        {" "}
+        Add A New Flash Card
+      </button>
     </div>
   );
 }
